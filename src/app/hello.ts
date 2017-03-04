17af3b8a28f8611ce837df5755e071d9c43dda22
Francisco Javier Rodríguez Olvera <frodriguez@netintellego.com>
2017-03-03 18:01:08 -0600

    Se agrega conexion con camara

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ViewChildren, QueryList, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'fountain-app',
@@ -6,9 +6,39 @@ import {Component} from '@angular/core';
   templateUrl: 'hello.html'
 })
 export class HelloComponent {
+  @ViewChild('myname') input: ElementRef;
+  @ViewChild('video') video: ElementRef;
+
+  @ViewChildren('div1,div2,div3') divs: QueryList<ElementRef>;
+
   public hello: string;
 
   constructor() {
     this.hello = 'Hello World!';
   }
+
+  ngAfterViewInit() {
+    var cameras = [];
+    var video = this.video;
+    navigator.mediaDevices.enumerateDevices()
+    .then(function(devices: any[]) {
+      cameras = devices.filter(d => d.kind === 'videoinput');
+      var currentSource = 0;
+      var videosources = [];
+      var lastStream;
+      let _video = video.nativeElement;
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: {deviceId: cameras[1].deviceId }, audio: false })
+                              .then(stream => {
+                                _video.src = window.URL.createObjectURL(stream);
+                                _video.play();
+                              });
+      }
+    })
+    .catch(function(err: string) {
+      console.log(err.name + ': ' + err.message);
+    });
+
+  }
+
 }
